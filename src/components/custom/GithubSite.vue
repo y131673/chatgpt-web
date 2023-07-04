f01367a3d8de775432d5a4bc73974209b3e83321
y131673 <138565612+y131673@users.noreply.github.com>
2023-07-04 16:52:41 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <a href="#" target="_blank" class="text-blue-500">
+      江苏策翼传媒有限公司
     </a>
   </div>
 </template>
