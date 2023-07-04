0e86778577cbd773317037c51e201ffd651372f0
y131673 <138565612+y131673@users.noreply.github.com>
2023-07-04 16:50:01 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -46,18 +46,18 @@ onMounted(() => {
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
+          江苏策翼传媒有限公司
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="#"
             target="_blank"
           >
-            GitHub
+            江苏策翼传媒有限公司
           </a>
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          江苏策翼传媒有限公司
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 GitHub 帮我点个 Star 或者给予一点赞助，谢谢！
+          江苏策翼传媒有限公司
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
